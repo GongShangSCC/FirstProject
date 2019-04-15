b70778739015cbf918464c7120dcad7d14bff2a1
Leon Tinashe Kanyayi <leonkanyayi@yahoo.com>
2019-04-14 18:14:03 -0700

    Update logic.c

--- Snake/Display/logic.c
+++ Snake/Display/logic.c
@@ -18,3 +18,36 @@ void Input() {
 		}
 	}
 }
+
+void Logic()
+{
+	switch (dir)
+	{
+        case LEFT:
+            snakeX--;
+            break;
+        case RIGHT:
+            snakeX++;
+            break;
+        case UP:
+            snakeY--;
+            break;
+        case DOWN:
+            snakeY++;
+            break;
+	}
+	if (snakeX >= WIDTH)
+		snakeX = 0;
+	if (snakeX < 0)
+		snakeX = WIDTH;
+	if (snakeY >= HEIGHT)
+		snakeY = 0;
+	if (snakeY < 0)
+		snakeY = HEIGHT;
+
+	if (snakeX == fruitX && snakeY == fruitY) {
+		fruitX = rand() % WIDTH;
+		fruitY = rand() % HEIGHT;
+		score++;
+	}
+}
